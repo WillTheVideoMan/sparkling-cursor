9f5ee8b129aa2fff96874a36db7c12986d4ad4eb
WillTheVideoMan <williamhall1324@gmail.com>
2019-06-06 00:17:59 +0100

    Updates

---
+++ precache-manifest.13d18071acd586885d03e8d11cd66d8f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0ac4006300a1168407c3d5a46fa8286",
+    "url": "/sparkling-cursor/index.html"
+  },
+  {
+    "revision": "aaecb196a50b0f85eec4",
+    "url": "/sparkling-cursor/static/js/2.f5a9d49c.chunk.js"
+  },
+  {
+    "revision": "93281103d905f2a40052",
+    "url": "/sparkling-cursor/static/js/main.4439099a.chunk.js"
+  },
+  {
+    "revision": "507031ce18f723509dc4",
+    "url": "/sparkling-cursor/static/js/runtime~main.a613633c.js"
+  },
+  {
+    "revision": "f30091c78ae3e687ceecbb009a417c63",
+    "url": "/sparkling-cursor/static/media/raw_pride.f30091c7.svg"
+  }
+]);
