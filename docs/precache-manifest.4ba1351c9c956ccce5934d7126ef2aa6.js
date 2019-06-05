215548c52d4b3837f4a300193225b933ac9c9b6f
WillTheVideoMan <williamhall1324@gmail.com>
2019-06-06 00:03:27 +0100

    Build docs (live version).

---
+++ docs/precache-manifest.4ba1351c9c956ccce5934d7126ef2aa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f849c449b5bd571537a8bf5904c622a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aaecb196a50b0f85eec4",
+    "url": "/static/js/2.f5a9d49c.chunk.js"
+  },
+  {
+    "revision": "93281103d905f2a40052",
+    "url": "/static/js/main.4439099a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f30091c78ae3e687ceecbb009a417c63",
+    "url": "/static/media/raw_pride.f30091c7.svg"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import gay from "./raw_pride.svg";
+import pride from "./raw_pride.svg";
 
 ReactDOM.render(
   <>
     <App count={8} size={16} gravity={2.5} spread={25} decay={0.4} />
     <button>Click For Glitter</button>
-    <h2>RAW Pride Demo</h2>
-    <img src={gay} />
+    <img src={pride} alt="pride logo" />
   </>,
   document.getElementById("root")
 );
